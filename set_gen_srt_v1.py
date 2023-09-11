78dc405500334d01394afedf8fc59ce095e2e363
Antonio <51487951+duush86@users.noreply.github.com>
2023-09-11 17:35:15 -0500

    Update set_gen_srt_v1.py

--- set_gen_srt_v1.py
+++ set_gen_srt_v1.py
@@ -41,18 +41,9 @@ def break_sentences(max_chars, subs, alternative):
                     ("," in w['word'] and not firstword)):
                 # break sentence at: . ! ? or line length exceeded
                 # also break if , and not first word
-                # print(srt_start_time.second)
-                #print(content)
+              
                 str_end_time = convert_time_to_srt_format(w['endTime'])
                 
-                # sub = srt.Subtitle(index=idx,
-                #                    start=timedelta(0, srt_start_time.second, srt_start_time.microsecond, 0, srt_start_time.minute, srt_start_time.hour),
-                #                    end=timedelta(0, str_end_time.second, str_end_time.microsecond, 0,str_end_time.minute, str_end_time.hour),
-                #                    content=srt.make_legal_content(content))
-                # print(srt_start_time, str_end_time)
-                # print(timedelta(0, srt_start_time.second, srt_start_time.microsecond, 0, srt_start_time.minute, srt_start_time.hour),
-                #       timedelta(0, str_end_time.second, str_end_time.microsecond, 0,str_end_time.minute, str_end_time.hour), srt.make_legal_content(content))
-
                 subs.append(srt.Subtitle(index=idx,
                                            start=timedelta(0, srt_start_time.second, srt_start_time.microsecond, 0, srt_start_time.minute, srt_start_time.hour),
                                            end=timedelta(0, str_end_time.second, str_end_time.microsecond, 0,str_end_time.minute, str_end_time.hour),
@@ -100,7 +91,6 @@ def convert_time_to_srt_format(time_str):
     hours, seconds = divmod(seconds, 3600)
     minutes, seconds = divmod(seconds, 60)
     time_str = f"{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}"
-    # print(time_str)
     time_object = datetime.strptime(time_str, '%H:%M:%S,%f').time()
 
     # Format the time in HH:MM:SS,mmm format
